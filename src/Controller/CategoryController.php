f19a18facb835d88fb991b6ed9b5a4ec1516af28
seb7474 <sebsalero@hotmail.fr>
2020-03-29 19:12:37 +0200

    CategoryPage (CategoryController and templating) Display selected category with their sub-categories

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,21 +4,25 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Repository\CategoryRepository;
+use App\Repository\SubCategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
     /**
-     * @Route("/category/{id]", name="category")
+     * @Route("/category/{id}", name="category")
      */
-    public function index(int $id,CategoryRepository $categoryRepository)
+    public function index(Category $category,CategoryRepository $categoryRepository,SubCategoryRepository $subCategoryRepository)
     {
 
-
+        $categoryReturn = $categoryRepository->find($category);
+        $subCategorys = $subCategoryRepository->findBy(array('id' => $category));
 
         return $this->render('category/index.html.twig', [
-            'category' => '$category',
+            'category' => $categoryReturn,
+            'subCategorys' => $subCategorys
         ]);
+
     }
 }
