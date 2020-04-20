4316b64bbf44842f6fb2f98899ef676325f3b01c
seb7474 <sebsalero@hotmail.fr>
2020-04-20 10:55:43 +0200

    add new comment

--- src/Controller/TopicController.php
+++ src/Controller/TopicController.php
@@ -3,14 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Entity\Comment;
 use App\Entity\Topic;
 use App\Repository\CategoryRepository;
 use App\Repository\CommentRepository;
 use App\Repository\TopicRepository;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -20,14 +24,46 @@ class TopicController extends AbstractController
     /**
      * @Route("/topic/{id}", name="topic_show")
      */
-    public function show(Topic $topic, TopicRepository $topicRepository, CommentRepository $commentRepository)
+    public function show(Topic $topic, TopicRepository $topicRepository, CommentRepository $commentRepository, EntityManagerInterface $entityManager, Request $request, UserRepository $userRepository)
     {
+
+        $userConnected = $this->getUser();
+        $user = $userRepository->find($userConnected);
+
         $topicSelected = $topicRepository->find($topic);
         $comments = $commentRepository->findBy(array('topic'=>$topic));
+
+        $newComment = new Comment();
+
+        $form = $this->createFormBuilder($newComment)
+            ->add('content',TextType::class ,[
+                'label'=>'Commentaire'
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => "Ajouter"
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $newComment->setAuthor($user);
+            $newComment->setTopic($topicSelected);
+            $entityManager->persist($newComment);
+            $entityManager->flush();
+
+            return $this->redirectToRoute("topic_show",['id'=>1]);
+        }
+
+
+
 
         return $this->render('topic/show.html.twig', [
             'comments'=>$comments,
             'topic' => $topicSelected,
+            'commentForm' => $form->createView(),
+
         ]);
     }
 
