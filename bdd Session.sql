66735dce11479242d6d0aab1edbcaa220fe11ceb
Romain PIOT <romainpiot.pro@gmail.com>
2020-04-27 15:11:42 +0200

    add humouristic pokemon descriptions

---
+++ bdd Session.sql
@@ -0,0 +1,4 @@
+UPDATE "pokemon" SET description =
+"Dévore tous les insectes qu'il trouve sur son passage ! Oui oui, seulement les insectes. Quoi ton chien ? Un chihuahua c'est pas une race d'insecte ? ... RIP Caramel..."
+WHERE id_pokemon = 71
+
