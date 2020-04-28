626efa52e10aaa0e3ed3dc458c57b019f7b605ae
Romain PIOT <romainpiot.pro@gmail.com>
2020-04-28 22:05:39 +0200

    full css mapping with scss and adding apache package to symfony

--- bdd Session.sql
+++
@@ -1,4 +0,0 @@
-UPDATE "pokemon" SET description =
-"Dévore tous les insectes qu'il trouve sur son passage ! Oui oui, seulement les insectes. Quoi ton chien ? Un chihuahua c'est pas une race d'insecte ? ... RIP Caramel..."
-WHERE id_pokemon = 71
-
